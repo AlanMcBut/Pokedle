687186861c57707c7af205ac32e4687db1773fdf
AlanMcBut <alanmcbut@gmail.com>
2023-08-15 12:10:45 -0300

    creado eleccion usuario

--- pokedle.js
+++ pokedle.js
@@ -1,7 +1,25 @@
 function elegirPokemon() {
-    numeroPokeElegido = Math.round(Math.random() * 151 + 1 )
+    numeroPokeElegido = Math.floor(Math.random() * 151 + 1 )
     const eleccion = listaPokemon.find((el) =>  el.id === numeroPokeElegido)
-    
-    return console.log(eleccion)
+    console.log(eleccion);
+    return eleccion;
 }
-elegirPokemon();
+
+function eleccionUsuario() {
+    const eleccion = elegirPokemon();
+    let juegoCompletado = false;
+    
+    while (!juegoCompletado) {
+        let respuestaUsuario = prompt('Ingrese un pokemon: ');
+        respuestaUsuario = listaPokemon.find((el) => el.nombre === respuestaUsuario);
+        if(respuestaUsuario.id === eleccion.id){
+            alert('Adivinaste el pokemon, felicitaciones!');
+            juegoCompletado = true;
+        }
+        else {
+            alert('Incorrecto! Intenta otra vez:')
+           
+        }
+    }
+}
+eleccionUsuario();
